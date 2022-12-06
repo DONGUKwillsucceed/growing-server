0a720e4f074d08db538b3c32432f35b8729dfa18
Dong <rlaehddnr1009@naver.com>
2022-12-06 14:12:52 +0900

    채팅을 아카이빙하면 love Gauge가 올가게 하였음

--- src/module/chatting-archived/service/archive-chatting.service.ts
+++ src/module/chatting-archived/service/archive-chatting.service.ts
@@ -9,6 +9,11 @@ export class ArchiveChattingService {
   async archive(coupleId: string, userId: string, chattingId: string) {
     const data = this.createChattingStorageData(coupleId, userId, chattingId);
     await this.create(data);
+    await this.getPetID(coupleId).then(async (petId) => {
+      if (!(await this.isUsedStorage(petId))) {
+        await this.increasePetLoveGauge(petId);
+      }
+    });
   }
 
   async create(data: Prisma.ChattingStorageUncheckedCreateInput) {
@@ -29,5 +34,37 @@ export class ArchiveChattingService {
       coupleId,
     };
     return data;
+  }
+
+  async increasePetLoveGauge(id: string) {
+    await this.prismaService.pets.update({
+      where: { id },
+      data: {
+        loveGauge: {
+          increment: 2,
+        },
+      },
+    });
+
+    await this.prismaService.petCare.update({
+      where: { id },
+      data: { isUseStorage: 1 },
+    });
+  }
+
+  async getPetID(coupleId: string) {
+    const { id } = await this.prismaService.pets.findFirst({
+      where: { coupleId, isDeleted: 0 },
+      select: { id: true },
+    });
+    return id;
+  }
+
+  async isUsedStorage(id: string) {
+    const { isUseStorage } = await this.prismaService.petCare.findUnique({
+      where: { id },
+      select: { isUseStorage: true },
+    });
+    return isUseStorage;
   }
 }
