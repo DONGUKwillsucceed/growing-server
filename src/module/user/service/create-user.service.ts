c9bfdf8c9339d0d8a764098dc997527e6a1dbe20
Dong <rlaehddnr1009@naver.com>
2023-03-19 20:40:51 +0900

    유져가 가입하면 기본 이모지를 준다

--- src/module/user/service/create-user.service.ts
+++ src/module/user/service/create-user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { prisma, Prisma } from '@prisma/client';
 import { KakaoData } from 'src/module/auth/types/KakaoData';
+import { PrismaService } from 'src/service/prisma.service';
 import { v4 as uuidv4 } from 'uuid';
 import { UserCodeService } from './user-code.service';
 import { UserDBService } from './user-db.service';
@@ -10,12 +11,27 @@ export class CreateUserService {
   constructor(
     private readonly userDBService: UserDBService,
     private readonly userCodeService: UserCodeService,
+    private readonly prismaService: PrismaService,
   ) {}
 
   async createWithKakaoData(kakaoData: KakaoData) {
     const code = await this.userCodeService.generateOne();
     const data = this.createUserData(kakaoData, code);
-    return await this.userDBService.create(data);
+    const user = await this.userDBService.create(data);
+    const emojiPackIds = [
+      'bef7a678-e44b-4c80-8c5f-1b0af1848821',
+      '531f31ce-8450-46f8-8b32-bc2e87fb5a5c',
+      'f41962ba-caa9-4c82-ba37-ff7683421343',
+    ];
+    for (const packId of emojiPackIds) {
+      await this.prismaService.emoji_Order.create({
+        data: {
+          id: uuidv4(),
+          buyerId: user.id,
+          emojiPackId: packId,
+        },
+      });
+    }
   }
 
   private createUserData(kakaoData: KakaoData, code: string) {
