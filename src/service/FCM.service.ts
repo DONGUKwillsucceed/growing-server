57b2ad1d1253401346afb93a2eb54d89265daea2
Dong <rlaehddnr1009@naver.com>
2023-05-16 02:50:08 +0900

    FCM 구동을 위한 FCMService를 제작했음

---
+++ src/service/FCM.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as firebase from 'firebase-admin';
+
+export interface ISendFirebaseMessages {
+  token: string;
+  title?: string;
+  message: string;
+}
+
+@Injectable()
+export class FCMService {
+  logger = new Logger(FCMService.name);
+  constructor() {
+    // For simplicity these credentials are just stored in the environment
+    // However these should be stored in a key management system
+    const firebaseCredentials = JSON.parse(
+      process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    );
+    firebase.initializeApp({
+      credential: firebase.credential.cert(firebaseCredentials),
+    });
+  }
+
+  async sendMessages(fCMs: ISendFirebaseMessages) {
+    await firebase
+      .messaging()
+      .send(fCMs)
+      .then((res) =>
+        this.logger.log(`${fCMs.title}이 발송되었습니다. \n ${res}`),
+      )
+      .catch((err) => {
+        this.logger.log(`${fCMs.title}의 전송에 실패했습니다. \n ${err}`);
+        throw new Error(err);
+      });
+  }
+}
